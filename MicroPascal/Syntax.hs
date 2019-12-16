4bbddf6826854ab29dfc8f159185e43ab084a766
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-12-16 17:16:50 -0300

    Add more program examples

--- MicroPascal/Syntax.hs
+++ MicroPascal/Syntax.hs
@@ -86,7 +86,10 @@ $(mkSemFuncs [''Nt_Program, ''Nt_Body, ''Nt_StmtList,
             ''Nt_DefList, ''Nt_Defs, ''Nt_Stmt, ''Nt_Expr])
 
 
+-- Simplest program
 prog1 = Program "ejemplo1" (Defs EmptyDef) (Body EmptyStmt)
+
+-- Variable declaration and assignment
 prog2 = Program "ejemplo2" (Defs (ConsDef "x" TInt (Defs (ConsDef "y" TInt (Defs (ConsDef "b" TBool (Defs EmptyDef)))))))
   (Body (ConsStmt (Assign "x" (NatL 10)) (
     ConsStmt (Assign "y" (BOpExpr (Var "x") OTimes (BOpExpr (NatL 3) OPlus (NatL 2)))) (
@@ -97,3 +100,71 @@ prog2 = Program "ejemplo2" (Defs (ConsDef "x" TInt (Defs (ConsDef "y" TInt (Defs
       )
     )
   ))
+
+-- If example
+prog3 = Program "ejemplo3" (Defs (ConsDef "w" TInt (Defs (ConsDef "x" TBool (Defs EmptyDef)))))
+  (Body (ConsStmt (ReadLn "w") (
+    ConsStmt (Assign "x" (BOpExpr (Var "w") OLT (NatL 10))) (
+      ConsStmt (If (Var "x") (Body (ConsStmt (WriteLn (Var "w")) EmptyStmt)) (Body (ConsStmt (WriteLn (NatL 10)) EmptyStmt))) EmptyStmt
+      )
+    )
+  ))
+
+-- While example
+prog4 = Program "ejemplo4" (Defs (ConsDef "x" TInt (Defs EmptyDef)))
+  (Body (ConsStmt (ReadLn "x") (
+    ConsStmt (While (BOpExpr (NatL 0) OLT (Var "x")) (Body (ConsStmt (WriteLn (Var "x")) (ConsStmt (Assign "x" (BOpExpr (Var "x") OMinus (NatL 1))) EmptyStmt)))) EmptyStmt
+    )
+  ))
+
+-- Duplicated definitions
+prog5 = Program "ejemplo5" (Defs (ConsDef "x" TInt (Defs (ConsDef "y" TBool (Defs (ConsDef "y" TInt (Defs (ConsDef "y" TInt (Defs (ConsDef "x" TInt (Defs (ConsDef  "y" TBool (Defs EmptyDef)))))))))))))
+  (Body (ConsStmt (ReadLn "y") (
+    ConsStmt (Assign "x" (BOpExpr (Var "x") OPlus (Var "y"))) EmptyStmt
+    )
+  ))
+
+-- Undefined names
+prog6 = Program "ejemplo6" (Defs (ConsDef "x" TInt (Defs (ConsDef "y" TInt (Defs EmptyDef)))))
+  (Body (ConsStmt (ReadLn "z") (
+    ConsStmt (Assign "i" (NatL 1)) (
+      ConsStmt (While (BOpExpr (Var "i") OLT (NatL 10)) (Body (ConsStmt (Assign "i" (BOpExpr (Var "i") OPlus (NatL 1))) EmptyStmt))) EmptyStmt
+      )
+    )
+  ))
+
+-- Type errors
+prog7 = Program "ejemplo7" (Defs (ConsDef "b" TBool (Defs (ConsDef "x" TInt (Defs EmptyDef)))))
+  (Body (ConsStmt (ReadLn "b") (
+    ConsStmt (ReadLn "x") (
+      ConsStmt (If (BOpExpr (Var "b") OAnd (Var "x")) (Body (ConsStmt (WriteLn (Var "x")) EmptyStmt)) (Body (ConsStmt (While (BOpExpr (Var "x") OTimes (NatL 4)) (Body (ConsStmt (WriteLn (Var "b")) EmptyStmt))) EmptyStmt))) (
+        ConsStmt (Assign "x" (BOpExpr (BOpExpr (Var "x") OPlus (Bool True)) OAnd (BOpExpr (Var "b") OOr (NatL 8)))) EmptyStmt
+      )
+    )
+  )))
+
+-- Program to optimize
+prog8 = Program "ejemplo8" (Defs (ConsDef "x" TInt (Defs EmptyDef)))
+  (Body (ConsStmt (ReadLn "x") (
+    ConsStmt (Assign "x" (BOpExpr (NatL 2) OPlus (BOpExpr (Var "x") OTimes (NatL 0)))) (
+      ConsStmt (Assign "x" (BOpExpr (BOpExpr (NatL 3) OPlus (NatL 2)) OTimes (Var "x"))) (
+        ConsStmt (Assign "x" (BOpExpr (Var "x") OPlus (BOpExpr (NatL 0) ODiv (NatL 3)))) (
+          ConsStmt (If (BOpExpr (BOpExpr (Var "x") OEq (NatL 38)) OAnd (Bool False)) (Body (ConsStmt (WriteLn (Var "x")) EmptyStmt)) (Body (ConsStmt (WriteLn (BOpExpr (Var "x") OPlus (NatL 1))) EmptyStmt))) (
+            ConsStmt (While (BOpExpr (BOpExpr (NatL 2) OEq (NatL 3)) OOr (Bool False)) (Body (ConsStmt (WriteLn (Var "x")) EmptyStmt))) EmptyStmt
+          )
+        )
+      )
+    )
+  )))
+
+-- Optimized program
+prog8' = Program "ejemplo8'" (Defs (ConsDef "x" TInt (Defs EmptyDef)))
+  (Body (ConsStmt (ReadLn "x") (
+    ConsStmt (Assign "x" (NatL 2)) (
+      ConsStmt (Assign "x" (BOpExpr (NatL 5) OTimes (Var "x"))) (
+        ConsStmt (Assign "x" (Var "x")) (
+          ConsStmt (WriteLn (BOpExpr (Var "x") OTimes (NatL 1))) EmptyStmt
+        )
+      )
+    )
+  )))
