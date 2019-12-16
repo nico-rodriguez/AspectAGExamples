ba2a3ee7b54722ebae13521c1311dd6daa5ebc44
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-12-15 23:30:59 -0300

    Revert some changes

--- MicroPascal/Syntax.hs
+++ MicroPascal/Syntax.hs
@@ -14,14 +14,6 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
--- {-# OPTIONS_GHC -O0 #-}
--- {-# OPTIONS_GHC +RTS #-}
--- {-# OPTIONS_GHC -A128m #-}
--- {-# OPTIONS_GHC -n8m #-}
--- {-# OPTIONS_GHC -s #-}
--- {-# OPTIONS_GHC -RTS #-}
--- {-# OPTIONS_GHC -j2 #-}
-
 
 module MicroPascal.Syntax where
 
@@ -99,20 +91,15 @@ $(mkSemFuncs [''Nt_Program, ''Nt_Body, ''Nt_StmtList,
 $(attLabel "sshow" ''String)
 
 sshow_Program     = syndefM sshow p_Program $ showProgram <$> ter ch_programName <*> at ch_programDefs sshow <*> at ch_programBody sshow
-  where showProgram n d b = "Program " ++ n ++ a ++ ";\n" ++ d ++ b
+  where showProgram n d b = "Program " ++ n ++ ";\n" ++ "Var\n" ++ d ++ "Begin\n" ++ b ++ "End.\n"
 sshow_EmptyDef    = syndefM sshow p_EmptyDef $ pure ""
 sshow_ConsDef     = syndefM sshow p_ConsDef $ showConsDef <$> ter ch_varName <*> (show <$> ter ch_varType) <*> at ch_tailDefList sshow
   where showConsDef n t d = n ++ " : " ++ t ++ ";\n" ++ d
-sshow_Defs        = syndefM sshow p_Defs $ showDefs <$> at ch_defList sshow
-  where
-    showDefs dl@ConsDef{} = "Var " ++ dl
-    showDefs EmptyDef     = "Var\n"
-
+sshow_Defs        = syndefM sshow p_Defs $ at ch_defList sshow
 sshow_ConsStmt    = syndefM sshow p_ConsStmt $ showConsStmt <$> at ch_headStmt sshow <*> at ch_tailStmtList sshow
   where showConsStmt h t = h ++ t
 sshow_EmptyStmt  = syndefM sshow p_EmptyStmt $ pure ""
-sshow_Body        = syndefM sshow p_Body $ showBody <$> at ch_bodyStmts sshow
-  where showBody b = "Begin\n" ++ b ++ "End.\n"
+sshow_Body        = syndefM sshow p_Body $ at ch_bodyStmts sshow
 sshow_Assign      = syndefM sshow p_Assign $ showAssign <$> ter ch_assignName <*> at ch_assignExpr sshow
   where showAssign n e = n ++ " := " ++ e ++ ";\n"
 sshow_If          = syndefM sshow p_If $ showIf <$> at ch_ifCond sshow <*> at ch_ifThen sshow <*> at ch_ifElse sshow
@@ -123,7 +110,7 @@ sshow_WriteLn     = syndefM sshow p_WriteLn $ showwriteLn <$> at ch_writeLnExpr 
   where showwriteLn e = "WriteLn " ++ e ++ ";\n"
 sshow_ReadLn      = syndefM sshow p_ReadLn $ showReadLn <$> ter ch_readLnName
   where showReadLn n = "ReadLn " ++ n ++ ";\n"
-sshow_Var         = syndefM sshow p_Var  $ (ter ch_litName)
+sshow_Var         = syndefM sshow p_Var  $ ter ch_litName
 sshow_Bool        = syndefM sshow p_Bool $ show <$> ter ch_litBool
 sshow_NatL        = syndefM sshow p_NatL $ show <$> ter ch_litNat
 sshow_BOpExpr     = syndefM sshow p_BOpExpr $ wrap <$> at ch_l sshow <*> (show <$> ter ch_bop) <*> at ch_r sshow
